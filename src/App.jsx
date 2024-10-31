e87c0bc12f429efd0b3d7935234dfb1141c14ef6
DavSan <david.sanchez.martinez@outlook.com>
2024-10-31 19:44:14 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ export default function App({ darkMode }) {
     const [boxesArray, setBoxesArray] = React.useState(boxes)
 
 
-    const styles = {
-        backgroundColor: darkMode ? "#222222" : "#cccccc"
-    }
+    // const styles = {
+    //     backgroundColor: darkMode ? "#222222" : "#cccccc"
+    // }
 
     const boxElements = boxesArray.map((box) => (
         // <div style={styles} key={box.id} className="box"></div>
-        <Box on={box.on}/>
+        <Box on={box.on} key={box.id} className="box"/>
     ))
 
 
