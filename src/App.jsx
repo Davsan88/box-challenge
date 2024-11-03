b6b0bea45704045d7aed9ebfe8acbad4fe01c1a2
DavSan <david.sanchez.martinez@outlook.com>
2024-11-03 18:28:33 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,8 @@ export default function App({ darkMode }) {
     const boxElements = boxesArray.map((box) => (
         <Box 
             key={box.id} 
-            id={box.id}
             on={box.on} 
-            toggle={toggle}
+            toggle={() => toggle(box.id)}
         />
     ))
 
